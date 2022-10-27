a0fc08b608e3ce4d6f318d81d56b266123a5fc52
Pablo Gonzálvez del Pino <pablogonzalvezdelpino99@gmail.com>
2022-10-27 16:26:06 +0200

    Initial Commit

---
+++ Twitter_Pablo/Twitter_PabloApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_PabloApp.swift
+//  Twitter_Pablo
+//
+//  Created by Apps2T on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_PabloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
